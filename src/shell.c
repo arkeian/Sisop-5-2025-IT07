b126fc59f94d65af0524d288df2dd9367a982b6c
M. Rakha Hananditya R. <rakhahananditya4521@gmail.com>
2025-06-03 16:30:26 +0700

    shell_v1.0.2

--- src/shell.c
+++ src/shell.c
@@ -1,9 +1,8 @@
 // shell_with_history_cd.c
 
 #include "shell.h"
-#include "kernel.h"
 #include "std_lib.h"
-#include <string.h>
+#include "kernel.h"
 
 #define MAX_HISTORY 20
 
@@ -12,8 +11,6 @@ char history[MAX_HISTORY][128];
 int history_count = 0;
 
 static void parseCommand(char *buf, char *cmd, char arg[10][64], int *arg_count);
-static int string_compare(const char *s1, const char *s2);
-static void string_copy(char *dest, const char *src);
 
 void shell() {
     char buf[128];
@@ -29,19 +26,19 @@ void shell() {
         printString("> ");
 
         readString(buf);
-        string_copy(original_input, buf);
+        strcpy(original_input, buf);
 
         // Simpan ke history (jika tidak kosong)
         if (buf[0] != '\0') {
             if (history_count < MAX_HISTORY) {
-                string_copy(history[history_count++], buf);
+                strcpy(history[history_count++], buf);
             } else {
                 // Geser seluruh history ke bawah, buang index 0, tambahkan di akhir
                 int i;
                 for (i = 0; i < MAX_HISTORY-1; i++) {
-                    string_copy(history[i], history[i+1]);
+                    strcpy(history[i], history[i+1]);
                 }
-                string_copy(history[MAX_HISTORY-1], buf);
+                strcpy(history[MAX_HISTORY-1], buf);
             }
         }
 
@@ -50,14 +47,14 @@ void shell() {
         if (cmd[0] == '\0') continue;
 
         // exit / quit
-        if (string_compare(cmd, "exit") == 0 || string_compare(cmd, "quit") == 0) {
+        if (strcmp(cmd, "exit") == 0 || strcmp(cmd, "quit") == 0) {
             printString("Bye, ");
             printString(current_user);
             printString("!\n");
             break;
         }
         // help
-        else if (string_compare(cmd, "help") == 0) {
+        else if (strcmp(cmd, "help") == 0) {
             printString("Available commands:\n");
             printString("  user [name]    : Change current username\n");
             printString("  cd [dir]       : Change directory\n");
@@ -69,31 +66,19 @@ void shell() {
             printString("  (else)         : Echo input\n");
         }
         // user
-        else if (string_compare(cmd, "user") == 0) {
+        else if (strcmp(cmd, "user") == 0) {
             if (arg_count >= 1 && args[0][0] != '\0') {
-                string_copy(current_user, args[0]);
+                strcpy(current_user, args[0]);
                 printString("Username changed to ");
                 printString(current_user);
             } else {
-                string_copy(current_user, "user");
+                strcpy(current_user, "user");
                 printString("Username changed to user");
             }
             printString("\n");
         }
-        // cd
-        else if (string_compare(cmd, "cd") == 0) {
-            if (arg_count >= 1 && args[0][0] != '\0') {
-                int res = changeDirectory(args[0]); // asumsikan syscall
-                if (res < 0) {
-                    printString("cd: Failed to change directory\n");
-                }
-            } else {
-                // tanpa argumen, balik ke home (misal root "/")
-                changeDirectory("/");
-            }
-        }
         // history
-        else if (string_compare(cmd, "history") == 0) {
+        else if (strcmp(cmd, "history") == 0) {
             int i;
             for (i = 0; i < history_count; i++) {
                 printString(history[i]);
@@ -101,9 +86,9 @@ void shell() {
             }
         }
         // yo dan gurt
-        else if (string_compare(cmd, "yo") == 0) {
+        else if (strcmp(cmd, "yo") == 0) {
             printString("gurt\n");
-        } else if (string_compare(cmd, "gurt") == 0) {
+        } else if (strcmp(cmd, "gurt") == 0) {
             printString("yo\n");
         }
         // else echo
@@ -146,22 +131,4 @@ static void parseCommand(char *buf, char *cmd, char arg[10][64], int *arg_count)
 
         while (buf[i] != '\0' && buf[i] == ' ') i++;
     }
-}
-
-// Jika ingin pakai custom string compare/copy, gunakan di bawah ini:
-static int string_compare(const char *s1, const char *s2) {
-    int i = 0;
-    while (s1[i] != '\0' && s2[i] != '\0') {
-        if (s1[i] != s2[i]) return (int)(s1[i] - s2[i]);
-        i++;
-    }
-    return (int)(s1[i] - s2[i]);
-}
-
-static void string_copy(char *dest, const char *src) {
-    int i = 0;
-    while (src[i] != '\0') {
-        dest[i] = src[i++];
-    }
-    dest[i] = '\0';
 }
