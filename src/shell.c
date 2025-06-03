d4ae264b707f8a61ba4a287127f9017b2cf90707
jagosyafaat30 <syafaatmm30@gmail.com>
2025-06-03 13:20:19 +0700

    Update shell.c

--- src/shell.c
+++ src/shell.c
@@ -14,7 +14,38 @@ void shell(){
 }
 
 void parseCommand(char *buf, char *cmd, char arg[2][64]) {
-  //TODO: Implementasi fungsi untuk mem-parsing perintah
+  int i = 0;
+    int j = 0;
+    int arg_index = 0;
+
+    while (buf[i] == ' ' && buf[i] != '\0') i++;
+
+    j = 0;
+    while (buf[i] != '\0' && buf[i] != ' ' && j < 63) {
+        cmd[j] = buf[i];
+        i++;
+        j++;
+    }
+    cmd[j] = '\0';
+
+    while (buf[i] == ' ' && buf[i] != '\0') i++;
+
+    for (arg_index = 0; arg_index < 2; arg_index++) {
+        j = 0;
+        if (buf[i] == '\0') {
+            arg[arg_index][0] = '\0';
+            continue;
+        }
+        
+        while (buf[i] != '\0' && buf[i] != ' ' && j < 63) {
+            arg[arg_index][j] = buf[i];
+            i++;
+            j++;
+        }
+        arg[arg_index][j] = '\0';
+        
+        while (buf[i] == ' ' && buf[i] != '\0') i++;
+    }
 }
 
 // Tambahkan fungsi bila perlu
